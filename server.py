f047aaae6aea4b9f76920f7d51ecc6ee028d6bad
rhitabratakarar <rintu.karar@gmail.com>
2021-08-11 20:07:02 +0530

    chat is working, needed to add the database feature

--- server.py
+++ server.py
@@ -13,6 +13,14 @@ def index ():
 	# the basic front page of the server.
 	return render_template ("index.html")
 
+# when "client_message" occurs in client side
+@socketio.on ("client_message")
+def show (message_data):
+	print ("Server Received: ", message_data)
+
+	# send "my response" back to the client.
+	socketio.emit ("server_response", message_data)
+
 if __name__ == "__main__":
 	# socket created at default port = 5000
 	socketio.run (application, debug=True)

263c950472eb8c6512069c26cc96e065dfed7835
rhitabratakarar <kararrhitabrata@gmail.com>
2022-04-01 19:21:47 +0530

    fixed login and signup

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,51 @@
 window.onload = function() {
   document.getElementById("container").style.display = "flex";
 }
+
+
+function emptyPasswordFields() {
+  document.getElementById("password").value = "";
+  document.getElementById("con_pass").value = "";
+}
+
+function sendPostRequestToCurrentURL(data) {
+  var xhr = new XMLHttpRequest();
+  var result = document.getElementById("result");
+
+  xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      result.innerHTML = this.responseText;
+    }
+    else if(this.status == 500){
+      result.innerHTML = "Username Exists.";
+    }
+    else {
+      result.innerHTML = "Something Went Wrong.";
+    }
+  }
+
+  var url = window.location.href;
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+  xhr.send("username="+data["username"]+"&password="+data["password"]);
+}
+
+function validatePasswordAndSubmit() {
+
+  var givenPassword = document.getElementById("password").value;
+  var confPassword = document.getElementById("con_pass").value;
+  var username = document.getElementById("username").value;
+
+  if(givenPassword !== confPassword) {
+    alert("Passwords did not match, please re-enter.");
+  }
+
+  else {
+    data = {username: username, password: givenPassword};
+    sendPostRequestToCurrentURL(data);
+  }
+
+  emptyPasswordFields();
+}
+
+
